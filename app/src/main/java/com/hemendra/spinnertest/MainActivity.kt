1ae9027647ed3d715f76d3199b8f962d096977b6
hemu <hemendraoid>
2020-09-04 09:55:56 +0530

    first commit

---
+++ app/src/main/java/com/hemendra/spinnertest/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.hemendra.spinnertest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var courses_Array = arrayOf("AI","DS","ML","CNN","IOT","BLOCK CHAIN")
+
+        var arrayadapter = ArrayAdapter<String>(this@MainActivity,android.R.layout.simple_list_item_1,courses_Array)
+
+         spinner.adapter = arrayadapter
+         spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener
+
+            {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                //logic
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+
+                Toast.makeText(this@MainActivity,courses_Array[position],Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+}
+
+
+}
